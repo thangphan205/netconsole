16d9f89754385fbfcb8fea0161a0ac33ab5aaa9a
9Ping <thangphan205@gmail.com>
2024-05-29 11:31:09 +0700

    fix bug fetch interfaces when change switch

--- frontend/src/routes/_layout/interfaces.tsx
+++ frontend/src/routes/_layout/interfaces.tsx
@@ -20,7 +20,7 @@ import { createFileRoute } from "@tanstack/react-router"
 
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
-import { InterfacesService, SwitchesService, SwitchPublic } from "../../client"
+import { InterfacesService, SwitchesService } from "../../client"
 import ActionsMenu from "../../components/Common/ActionsMenu"
 import Navbar from "../../components/Common/Navbar"
 import { useState, ChangeEvent } from "react";
@@ -31,23 +31,24 @@ export const Route = createFileRoute("/_layout/interfaces")({
   component: Interfaces,
 })
 
+function InterfacesTableBody() {
 
-function storeList(key: string, list: SwitchPublic[]) {
-  localStorage.setItem(key, JSON.stringify(list));
-}
-
-function getList(key: string): SwitchPublic[] {
-  const storedList = localStorage.getItem(key);
-  return storedList ? JSON.parse(storedList) : [];
-}
-
-
-function SwitchInfo() {
   let switch_id = "0";
-  switch_id = String(localStorage.getItem("switch_id"));
-  const data_switches = getList("data_switches");
-  const switch_id_current = String(localStorage.getItem("switch_id_current"));
+  if (localStorage.getItem("switch_id")) {
+    switch_id = String(localStorage.getItem("switch_id"));
+  }
+
   const [selectedValue, setSelectedValue] = useState<string | undefined>(switch_id);;
+
+  const { data: switches } = useSuspenseQuery({
+    queryKey: ["switches"],
+    queryFn: async () => await SwitchesService.readSwitches({}),
+  })
+
+  const { data: interfaces } = useSuspenseQuery({
+    queryKey: ["interfaces", selectedValue],
+    queryFn: async () => await InterfacesService.readInterfaces({ switchId: Number(switch_id) }),
+  })
 
   const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
     setSelectedValue(event.target.value);
@@ -55,78 +56,66 @@ function SwitchInfo() {
   };
 
   return (
-    <FormControl mt={4}>
-      <InputGroup>
-        <InputLeftAddon>Switch: </InputLeftAddon>
-        <Select
-          placeholder="Select Switch"
-          value={selectedValue}
-          onChange={handleSelectChange}
-        >
-          {
-            data_switches ? (data_switches.map((item) => (
-              item.id === Number(switch_id_current) ?
-                (<option key={item.id} value={item.id} style={{ color: "blue" }}>Current data: {item.hostname} - {item.ipaddress}</option>)
-                : (<option key={item.id} value={item.id} style={{ color: "red" }}>Need refresh Data: {item.hostname} - {item.ipaddress}</option>)
-            ))) : null
-          }
-        </Select>
-      </InputGroup>
-    </FormControl>
-  )
-}
-function InterfacesTableBody() {
-
-  let switch_id = "0";
-  if (localStorage.getItem("switch_id")) {
-    switch_id = String(localStorage.getItem("switch_id"));
-  }
-  const { data: interfaces } = useSuspenseQuery({
-    queryKey: ["interfaces"],
-    queryFn: async () => await InterfacesService.readInterfaces({ switchId: Number(switch_id) }),
-  })
-  const { data: switches } = useSuspenseQuery({
-    queryKey: ["switches"],
-    queryFn: async () => await SwitchesService.readSwitches({}),
-  })
-  storeList("data_switches", switches.data);
-  localStorage.setItem("switch_id_current", switch_id)
-  return (
-
-    <Tbody>
-      {interfaces.data.map((item) => (
-        <Tr key={item.id}>
-          <Td>{item.id}</Td>
-          <Td>{item.port}</Td>
-          <Td>{item.description}</Td>
-          <Td>{item.status}</Td>
-          <Td>{item.vlan}</Td>
-          <Td>{item.mode}</Td>
-          <Td>{item.speed}</Td>
-          <Td>
-            <ActionsMenu type={"Interface"} value={item} />
-          </Td>
+    <>
+      <Thead>
+        <Tr>
+          <Th colSpan={8}>
+            <FormControl mt={4}>
+              <InputGroup>
+                <InputLeftAddon>Switch: </InputLeftAddon>
+                <Select
+                  placeholder="Select Switch"
+                  value={selectedValue}
+                  onChange={handleSelectChange}
+                >
+                  {
+                    switches ? (switches.data.map((item) => (
+                      item.id === Number(switch_id) ?
+                        (<option key={item.id} value={item.id} style={{ color: "blue" }}>Current data: {item.hostname} - {item.ipaddress}</option>)
+                        : (<option key={item.id} value={item.id} style={{ color: "red" }}>Need refresh Data: {item.hostname} - {item.ipaddress}</option>)
+                    ))) : null
+                  }
+                </Select>
+              </InputGroup>
+            </FormControl>
+          </Th>
         </Tr>
-      ))}
-    </Tbody>
+        <Tr>
+          <Th>ID</Th>
+          <Th>Port</Th>
+          <Th>Description</Th>
+          <Th>Status</Th>
+          <Th>Vlan</Th>
+          <Th>Mode</Th>
+          <Th>Speed</Th>
+          <Th>Actions</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {interfaces.data.map((item) => (
+          <Tr key={item.id}>
+            <Td>{item.id}</Td>
+            <Td>{item.port}</Td>
+            <Td>{item.description}</Td>
+            <Td>{item.status}</Td>
+            <Td>{item.vlan}</Td>
+            <Td>{item.mode}</Td>
+            <Td>{item.speed}</Td>
+            <Td>
+              <ActionsMenu type={"Interface"} value={item} />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+
+    </>
   )
 }
 function InterfacesTable() {
+
   return (
     <TableContainer>
       <Table size={{ base: "sm", md: "md" }}>
-        <Thead>
-          <Tr>
-            <Th>ID</Th>
-            <Th>Port</Th>
-            <Th>Description</Th>
-            <Th>Status</Th>
-            <Th>Vlan</Th>
-            <Th>Mode</Th>
-            <Th>Speed</Th>
-            <Th>Actions</Th>
-          </Tr>
-        </Thead>
         <ErrorBoundary
           fallbackRender={({ error }) => (
             <Tbody>
@@ -168,7 +157,6 @@ function Interfaces() {
         Interfaces Management
       </Heading>
       <Navbar type={"Interface"} />
-      <SwitchInfo />
       <InterfacesTable />
     </Container>
   )
